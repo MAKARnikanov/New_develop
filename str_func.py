ff1049f189e9b9e370dce9f75b4edcabd85a91c7
MAKARnikanov <maka.nik.00@mail.ru>
2023-02-12 14:55:46 +0300

    Create another one function tittlestring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def upperstring(str):
     str = 'super'
     return str.upper()
+
+def tittlestring(str):
+    str = 'and those that tasted the bite of his sword named him the doom slayer'
+    return str.title()
